bfe9aeb69e04c0934b35a09eaa893864c5dcce8f
xtxChino <2656823393@qq.com>
2025-01-09 12:04:38 +0800

    至少数据移植是成功的

--- PSoC 5LP CY8CKIT-101.cydsn/piano.h
+++ PSoC 5LP CY8CKIT-101.cydsn/piano.h
@@ -1,16 +1,6 @@
 #include "project.h"
-#define TONE 1
+#define TONE 3
 #define NOTEPERTONE 7
 #define LAYERS TONE *NOTEPERTONE + 1
 
-
-extern const uint8 CYCODE DACData_C4[48][50];
-extern const uint8 CYCODE DACData_D4[48][50];
-extern const uint8 CYCODE DACData_E4[48][50];
-extern const uint8 CYCODE DACData_F4[48][50];
-extern const uint8 CYCODE DACData_G4[48][50];
-extern const uint8 CYCODE DACData_A4[48][50];
-extern const uint8 CYCODE DACData_B4[48][50];
-extern const uint8 CYCODE DACData_C5[48][50];
-extern uint8 CYCODE allN1ote[LAYERS][48][50];
 extern const uint8 CYCODE allNoteNew[LAYERS][48][50];
